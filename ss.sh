42c90a4e29fd6b875ac7fba18ea317a6cb99b3a2
kusinalang01 <alang.kusin@gmail.com>
2024-12-27 02:00:28 +0700

    Update ss.sh

--- ss.sh
+++ ss.sh
@@ -1,1 +1,1 @@
-./bash --pool stratum+tcp://0xbaf57a71CF8bC88Df4F350595BBA77a8d9390d2C.Worker001@pool-core-testnet.inichain.com:32672
+./bash --pool stratum+tcp://0xde1d9fd7ff6fccf6179244ee238954e2c7dfbf10.alangkusin@pool-core-testnet.inichain.com:32672
